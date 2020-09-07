6d5ea4981efad6c8f22ddfd70dee08e148adfe7d
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-09-07 17:12:22 +0800

    JavaReview

---
+++ Java_入门_01/src/chairMan.java
@@ -0,0 +1,11 @@
+/**
+ * @Author: Jeffrey
+ * @Description:
+ * @Date: create in 2020/9/7 下午4:59
+ * @Version：JDK1.8
+ */
+public class chairMan {
+    public static void main(String[] args){
+        System.out.println("姓名：习大大\n\n性别：男\n家庭住址：北京");
+    }
+}
